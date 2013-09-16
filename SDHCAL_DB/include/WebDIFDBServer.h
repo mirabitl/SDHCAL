509bcfa6e4a41f316df4c52ea89121e9e63bceb1
mirabitl <laurent.mirabito@cern.ch>
2013-09-16 10:51:05 +0200

    DB server impelmentation

---
+++ SDHCAL_DB/include/WebDIFDBServer.h
@@ -0,0 +1,31 @@
+#ifndef WEBDIFDBSERVER_H
+#define WEBDIFDBSERVER_H
+#include <iostream>
+
+#include <string.h>
+#include<stdio.h>
+#include "NMServer.h"
+#include "OracleDIFDBManager.h"
+using namespace std;
+#include <sstream>
+class WebDIFDBServer: public NMServer
+{
+public:
+  WebDIFDBServer(std::string host,uint32_t port);
+  NetMessage* commandHandler(NetMessage* m);
+ 
+  void startServices();
+ 
+  void joinServices();
+  
+  void services();
+ 
+private:
+  bool running_;
+  boost::thread    m_Thread_s;
+  OracleDIFDBManager* theManager_;
+};
+
+
+
+#endif
