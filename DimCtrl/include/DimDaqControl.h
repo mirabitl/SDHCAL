55935a5be38af12f481b89acf77b3593d5089088
mirabito <laurent.mirabito@cern.ch>
2014-10-09 14:30:11 +0200

    SetThrehsold et gain

--- DimCtrl/include/DimDaqControl.h
+++ DimCtrl/include/DimDaqControl.h
@@ -48,6 +48,10 @@ public:
   void destroy();
   int getCurrentRun();
   char* getCurrentState();
+
+  void setGain(uint32_t g);
+  void setThresholds(uint32_t b0,uint32_t b1,uint32_t b2);
+
   void doScan(DimDDSClient* c);
   void doInitialise(DimDDSClient* c);
   void doRegisterstate(DimDDSClient* c);
@@ -55,6 +59,8 @@ public:
   void doStart(DimDDSClient* c);
   void doStop(DimDDSClient* c);
   void doDestroy(DimDDSClient* c);
+  void doSetGain(DimDDSClient* c);
+  void doSetThresholds(DimDDSClient* c);
   enum {ALIVED=1,BROWSED=2,SCANNED=3,INITIALISED=4,DBREGISTERED=5,CONFIGURED=6,STARTED=7,STOPPED=8,DESTROYED=9};
 private:
   std::string theDNS_;
@@ -72,6 +78,9 @@ private:
   char theCurrentState_[255];
   DimInfo* runInfo_;
   DimInfo* dbstateInfo_;
+
+  int32_t theCalibrationGain_;
+  int32_t theCalibrationThresholds_[3];
 };
 #endif
 
