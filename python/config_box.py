12d410de567124ed6c55612731c48ede9fd7422e
mirabito <laurent.mirabito@gmail.com>
2015-06-12 14:02:34 +0200

    update

---
+++ python/config_box.py
@@ -0,0 +1,22 @@
+host=['lyoilcrpi02']
+
+ccc='lyoilcrpi02'
+
+db='lyosdhcal11'
+writer='lyosdhcal11'
+zuphost='lyoilcrpi24'
+zupdevice='/dev/ttyUSB0'
+zupport=1
+
+#ilc sans PP
+#register=0x815A1B00
+#ilc avec PP
+register=0x89580000
+# BT PP
+#register=0x895A0040
+
+
+
+state="GIFSPS_43"
+directory="/data/NAS"
+monitor='lyosdhcal11'
