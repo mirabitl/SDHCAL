339bea83df26113e61382421e43cf05e7cab16cb
mirabitl <acqilc@localhost.localdomain>
2015-07-23 11:28:06 +0200

    mysql version

--- SlowControl/DS1820/include/DimDS1820Server.h
+++ SlowControl/DS1820/include/DimDS1820Server.h
@@ -8,6 +8,13 @@
 #include "dis.hxx"
 #include "DS1820.h"
 #include <sstream>
+#include <iostream>
+#include <string>
+#include "/opt/dhcal/dim/dim/dis.hxx"
+#include <boost/function.hpp>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include "MyInterface.h"
 
 using namespace std;
 
@@ -17,12 +24,25 @@ public:
   DimDS1820Server();
   ~DimDS1820Server();
 	void getTemperature();
-  
+  void Loop();
+  void readout();
+  void store();
+
+  void commandHandler();
+
 private:
   int32_t processStatus_;
   float readValues_[16];
   DimService* readService_; //State of the last register read 
- 
+  boost::thread_group g_d;
+
+  int32_t thePeriod_;
+  DimCommand *periodCommand_;
+  std::string theAccount_;
+  bool storeDb_;
+  MyInterface* my_;
+  DimCommand *storeCommand_;
+
   DS1820* theDS1820_;
 };
 #endif
--- SlowControl/DS1820/src/DimDS1820Server.cc
+++ SlowControl/DS1820/src/DimDS1820Server.cc
@@ -1,7 +1,7 @@
 
 #include "DimDS1820Server.h"
 
-DimDS1820Server::DimDS1820Server()
+DimDS1820Server::DimDS1820Server() : thePeriod_(10)
 {
 
   theDS1820_= new DS1820(); 
@@ -18,11 +18,47 @@ DimDS1820Server::DimDS1820Server()
 printf ("%s\n",s1.str().c_str());
   readService_ = new DimService(s0.str().c_str(),s1.str().c_str(),(float *)&readValues_, sizeof(readValues_ ));
 	readService_->updateService(); 
+s0.str(std::string());
+  s0<<"/DTEMP/"<<hname<<"/SETPERIOD";
+  periodCommand_=new DimCommand(s0.str().c_str(),"I:1",this);
 
+  s0.str(std::string());
+  s0<<"/DTEMP/"<<hname<<"/STORE";
+  storeCommand_=new DimCommand(s0.str().c_str(),"C",this);
+
+  
   s0.str(std::string());
   gethostname(hname,80);
   s0<<"DimDS1820Server-"<<hname;
-  DimServer::start(s0.str().c_str()); 
+  DimServer::start(s0.str().c_str());
+  Loop();
+
+}
+void DimDS1820Server::Loop()
+{
+  g_d.create_thread(boost::bind(&DimDS1820Server::readout, this));
+}
+
+void DimDS1820Server::store()
+{
+  if (!storeDb_) return;
+  if (my_==NULL) return;
+  my_->connect();
+  std::stringstream ss;
+  ss<<"INSERT INTO DS1820MON (TIN, TOUT) VALUES ("<<readValues_[0]<<","<<readValues_[1]<<")";
+
+  my_->executeQuery(ss.str());
+  my_->disconnect();
+}
+
+void DimDS1820Server::readout()
+{
+  while (true)
+    {
+      this->getTemperature();
+      this->store();
+      sleep((unsigned int) thePeriod_);
+    }
 }
 
 DimDS1820Server::~DimDS1820Server()
@@ -38,5 +74,21 @@ void DimDS1820Server::getTemperature()
 //	readValues_[1]=theDS1820_->tempC[1];
 	printf ("%f %f\n", readValues_[0], readValues_[1]);
 	readService_->updateService();
+
+}
+void DimDS1820Server::commandHandler()
+{
+  DimCommand *currCmd = getCommand();
+  printf(" J'ai recu %s COMMAND  \n",currCmd->getName());
+  if (currCmd==periodCommand_)
+    {
+      thePeriod_=currCmd->getInt();
+    }
+  if (currCmd==storeCommand_)
+    {
+      theAccount_.assign(currCmd->getString());
+      my_=new MyInterface(theAccount_);
+      storeDb_=true;
+    }
 
 }
