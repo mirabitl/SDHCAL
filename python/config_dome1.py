24e17a48729eb20bc02f81756acdb024a81c092d
mirabito <mirabito@lyosdhcal10.in2p3.fr>
2015-10-19 17:43:36 +0200

    Example of config files

---
+++ python/config_dome1.py
@@ -0,0 +1,32 @@
+#host=['lyoilcrpi18']
+
+#ccc='lyoilcrpi18'
+
+#db='lyoac29'
+#writer='lyoac29'
+jsonfile='/home/mirabito/SDHCAL/python/dome_oyonax_1.json'
+#zuphost='lyoilcrpi18'
+zupdevice='/dev/ttyUSB0'
+zupport=1
+
+#ilc sans PP
+#register=0x815A1B00
+
+#BT sans PP
+register=0x815A1B40
+
+#ilc avec PP
+#register=0x89580000
+
+# BT PP
+#register=0x895A0040
+
+# 0x815A1B40 2170166080 SDCC BT  lecture digitale        et temperatures 
+#register=0x815A1B40
+# 10= 110,120,120
+#11 = 180,120,120
+# 12 = 140 140 140
+
+state="oyonax_12"
+directory="/data/Local/oyonax"
+monitor='lyoac29'
