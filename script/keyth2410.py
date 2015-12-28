b40fa47c2d568ea9ffb15a1710b8f5b42403682c
acqilc <acqilc@localhost.localdomain>
2015-12-28 08:15:39 -0500

    Last version of keithley readout

--- script/keyth2410.py
+++ script/keyth2410.py
@@ -2,13 +2,14 @@
 import time
 import serial
 import os,sys
-if len(sys.argv) > 2:
+if len(sys.argv) > 3:
     fname=sys.argv[1]
     thick=float(sys.argv[2] )
+    NS=int(sys.argv[3] )
 else:
     fname='summaryiv'
     thick=0.2
-
+    NS=120
 surf=4.33736   
 
 
@@ -16,7 +17,7 @@ surf=4.33736
     
 # configure the serial connections (the parameters differs on the device you are connecting to)
 
-ser = serial.Serial("/dev/ttyUSB0")  # open first serial port
+ser = serial.Serial("/dev/ttyUSB1")  # open first serial port
 print ser.portstr       # check which port was really used
 ser.baudrate = 9600
 
@@ -44,15 +45,15 @@ lti=time.localtime()
 fl=open("%s_%d_%d_%d_%d_%d.txt" % (fname,lti.tm_year,lti.tm_mon,lti.tm_mday,lti.tm_hour,lti.tm_min),"w")
 
 T0=0
-
-for v in range(200,1001,100):
+#NS=60
+for v in range(90,1091,100):
     ser.write(':sour:volt %f\r\n' % v)
     time.sleep(1)
     V_MEAN=0
     I_MEAN=0
     R_MEAN=0
     RHO_MEAN=0
-    for i in range(0,120):
+    for i in range(0,NS):
         ser.write(':read?\r\n')
         vread=ser.readline().split(',')
         VM=float(vread[0])
@@ -64,16 +65,17 @@ for v in range(200,1001,100):
         RHO=RM*surf/thick
         print "%d,%g,%g,%g,%g,%f\n" % (i,VM,IM,RM,RHO,T-T0)
         fl.write("%d,%g,%g,%g,%g,%f\n" % (i,VM,IM,RM,RHO,T-T0))
-        V_MEAN=V_MEAN+VM/120.
-        I_MEAN=I_MEAN+IM/120.
-        R_MEAN=R_MEAN+RM/120.
-        RHO_MEAN=RHO_MEAN+RHO/120.        
+        fl.flush()
+        V_MEAN=V_MEAN+VM/(NS*1.)
+        I_MEAN=I_MEAN+IM/(NS*1.)
+        R_MEAN=R_MEAN+RM/(NS*1.)
+        RHO_MEAN=RHO_MEAN+RHO/(NS*1.)        
         time.sleep(1)
-    print "%d,%g,%g,%g,%g,%f\n" % (120,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0)
-    fl.write("%d,%g,%g,%g,%g,%f\n" % (120,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0))
+    print "%d,%g,%g,%g,%g,%f\n" % (NS,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0)
+    fl.write("%d,%g,%g,%g,%g,%f\n" % (NS,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0))
+    fl.flush()
 
-
-for v in range(900,1,-100): 
+for v in range(990,1,-100): 
     ser.write(':sour:volt %f\r\n' % v)
     time.sleep(1)
 
@@ -81,7 +83,7 @@ for v in range(900,1,-100):
     I_MEAN=0
     R_MEAN=0
     RHO_MEAN=0
-    for i in range(0,120):
+    for i in range(0,NS):
         ser.write(':read?\r\n')
         vread=ser.readline().split(',')
         VM=float(vread[0])
@@ -93,14 +95,15 @@ for v in range(900,1,-100):
         RHO=RM*surf/thick
         print "%d,%g,%g,%g,%g,%f\n" % (i,VM,IM,RM,RHO,T-T0)
         fl.write("%d,%g,%g,%g,%g,%f\n" % (i,VM,IM,RM,RHO,T-T0))
-        V_MEAN=V_MEAN+VM/120.
-        I_MEAN=I_MEAN+IM/120.
-        R_MEAN=R_MEAN+RM/120.
-        RHO_MEAN=RHO_MEAN+RHO/120.        
+        fl.flush()
+        V_MEAN=V_MEAN+VM/(NS*1.)
+        I_MEAN=I_MEAN+IM/(NS*1.)
+        R_MEAN=R_MEAN+RM/(NS*1.)
+        RHO_MEAN=RHO_MEAN+RHO/(NS*1.)        
         time.sleep(1)
-    print "%d,%g,%g,%g,%g,%f\n" % (120,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0)
-    fl.write("%d,%g,%g,%g,%g,%f\n" % (120,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0))
-
+    print "%d,%g,%g,%g,%g,%f\n" % (NS,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0)
+    fl.write("%d,%g,%g,%g,%g,%f\n" % (NS,V_MEAN,I_MEAN,R_MEAN,RHO_MEAN,0))
+    fl.flush()
 ser.write(':sour:volt 0.0\r\n')
 ser.write(':outp off\r\n')
 ser.close()
