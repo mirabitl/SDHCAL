ded576e50c44dfa96506e164ac3ffc7e2743679b
mirabitl <pi@lyoilcrpi24.in2p3.fr>
2016-02-26 15:18:04 +0000

    log4cxx support

--- SlowControl/bmp183/src/dbmp183.cc
+++ SlowControl/bmp183/src/dbmp183.cc
@@ -1,9 +1,18 @@
 #include "DimBMP183Server.h"
 #include <unistd.h>
 #include <stdint.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+using namespace log4cxx;
+using namespace log4cxx::xml;
+using namespace log4cxx::helpers;
 
 int main()
 {
+printf("parsing the config file \n");
+DOMConfigurator::configure("/etc/Log4cxxConfig.xml");
+//_logger->setLevel(log4cxx::Level::getInfo());
+LOG4CXX_INFO (_logger, "this is a info message, ignore.")
 DimBMP183Server* s=new DimBMP183Server();
   while (true)
 	{	
