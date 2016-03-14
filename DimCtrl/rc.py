699863fe20a155e93c15bab36c15db4c9b012f39
Laurent <laurent.mirabito@cern.ch>
2016-03-14 12:06:24 +0100

    oo

--- DimCtrl/rc.py
+++ DimCtrl/rc.py
@@ -2,8 +2,6 @@
 import os
 import socks
 import socket
-socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 1080)
-socket.socket = socks.socksocket
 import httplib, urllib,urllib2
 import json
 from copy import deepcopy
@@ -15,18 +13,35 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', action='store', dest='cmd',default='status',help='action to be done')
 parser.add_argument('-c', action='store', dest='config',default=None,help='python config file')
+parser.add_argument('--socks', action='store', dest='sockport',default=None,help='use SOCKS port ')
 parser.add_argument('--dbstate', action='store', default=None,dest='dbstate',help='set the dbstate')
-parser.add_argument('--ctrlreg', action='store', default=None,dest='ctrlreg',help='set the dbstate')
+parser.add_argument('--ctrlreg', action='store', default=None,dest='ctrlreg',help='set the dbstatectrreg in hexa')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 parser.add_argument('--state', action='store', type=str,default=None,dest='fstate',help='set the rpcdaq state')
 
 results = parser.parse_args()
+
+# Analyse results
+if (results.cmd == "forceState" and results.fstate==None):
+    print "please specify forceState with --fstate=new_state"
+    exit(0)
+if (results.config==None):
+    print "please specify a configuration with -c conf_name"
+    exit(0)
+
+if (results.sockport !=None):
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", results.sockport)
+    socket.socket = socks.socksocket
+    print "on utilise sock"
+
+
+# import the configuration
 try:
     exec("import %s  as conf" % results.config)
 except ImportError:
     raise Exception("cannot import")
 
- 
+# fill parameters 
 p_par={}
 if (conf.state!=None):
    p_par['dbstate']=conf.state
@@ -39,6 +54,20 @@ p_par['daqhost']=conf.daqhost
 p_par['daqport']=conf.daqport
 p_par['json']=conf.jsonfile
 
+# specific updates
+if (results.cmd != "setParameters" and results.dbstate!=None):
+    print "please use --dbstate=new_state with setParameters command"
+    exit(0)
+if (results.dbstate!=None):
+    p_par['dbstate']=results.dbstate
+
+if (results.cmd != "setParameters" and results.ctrlreg!=None):
+    print "please use --ctrlreg=new-reg (hexa) with setParameters command"
+    exit(0)
+
+if (results.ctrlreg!=None):
+    p_par['ctrlreg']=int(results.ctrlreg,16)
+    
 
 l_par=json.dumps(p_par,sort_keys=True)
 
@@ -65,16 +94,29 @@ def sendcommand2(command,host=p_par["daqhost"],port=p_par['daqport'],direc=None)
        print myurl
        req=urllib2.Request(myurl)
        r1=urllib2.urlopen(req)
-       if (command!="status"):
+       if (command=="status"):
+           s=r1.read()
+           sj=json.loads(s)
+           ssj=json.loads(sj["statusResponse"]["statusResult"][0])
+#           for x in ssj:
+#             for d in x["difs"]:
+#                print '#%4d %5x %6d %12d %12d %s %s ' % (d["id"],d["slc"],d["gtc"],d["bcid"],d["bytes"],d["state"],x["name"])
+           print "\033[1m %4s %5s %6s %12s %12s %15s  %s \033[0m" % ('DIF','SLC','EVENT','BCID','BYTES','SERVER','STATUS')
+
+           for x in ssj:
+              for d in x["difs"]:
+                  print '#%4d %5x %6d %12d %12d %15s %s ' % (d["id"],d["slc"],d["gtc"],d["bcid"],d["bytes"],x["name"],d["state"])
+       elif (command=="jobStatus"):
+           s=r1.read()
+           sj=json.loads(s)
+           ssj=json.loads(sj["jobStatusResponse"]["jobStatusResult"][0])
+           print "\033[1m %6s %15s %25s %20s \033[0m" % ('PID','NAME','HOST','STATUS')
+           for x in ssj:
+               if (x['DAQ']=='Y'):
+                   print "%6d %15s %25s %20s" % (x['PID'],x['NAME'],x['HOST'],x['STATUS'])
+       else:
           print r1.read()
           return r1.read()
-       else:
-          s=r1.read()
-          sj=json.loads(s)
-          ssj=json.loads(sj["statusResponse"]["statusResult"][0])
-          for x in ssj:
-             for d in x["difs"]:
-                print '#%4d %5x %6d %12d %12d %s %s ' % (d["id"],d["slc"],d["gtc"],d["bcid"],d["bytes"],d["state"],x["name"])
 
        
        #print r1.status, r1.reason
@@ -107,14 +149,13 @@ def sendcommand(command,host=p_par["daqhost"],port=p_par['daqport'],direc=None):
           sj=json.loads(s)
           ssj=json.loads(sj["statusResponse"]["statusResult"][0])
           for x in ssj:
-             for d in x["difs"]:
-                print '#%4d %5x %6d %12d %12d %s %s ' % (d["id"],d["slc"],d["gtc"],d["bcid"],d["bytes"],d["state"],x["name"])
+              print "\033[1m %4s %5s %6s %12s %12s %20s  %s \033[0m" % ('DIF','SLC','EVENT','BCID','SERVER','STATUS')
+              for d in x["difs"]:
+                  print '#%4d %5x %6d %12d %12d %s %s ' % (d["id"],d["slc"],d["gtc"],d["bcid"],d["bytes"],d["name"],x["state"])
 
        
           #print r1.status, r1.reason
-if (results.cmd == "forceState" and results.fstate==None):
-    print "please specify forceState with --fstate=new_state"
-    exit(0)
+
 if (results.cmd == "forceState"):
     sendcommand2(results.cmd,direc=results.fstate);
     exit(0)
