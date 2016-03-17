fac3df78a50db061610c6a8bdfc379dbe45a5ed2
laurent <you@example.com>
2016-03-17 09:12:31 +0100

    example pour la daq

---
+++ DimCtrl/config_m3_example.py
@@ -0,0 +1,19 @@
+#host=['lyoilcrpi03','lyoilcrpi04','lyoilcrpi05','lyoilcrpi06','lyoilcrpi07','lyoilcrpi08','lyoilcrpi09','lyoilcrpi10','lyoilcrpi11','lyoilcrpi12','lyoilcrpi14','lyoilcrpi15','lyoilcrpi19','lyoilcrpi13']
+#ccc='lyoilcrpi17'
+#db='lyosdhcal8'
+#writer='lyosdhcal8'
+
+jsonfile='/data/NAS/config/h2_m3_031016.json'
+zupdevice='/dev/ttyUSB0'
+zupport=1
+dccname="DCCCCC01"
+
+register=0x89580000
+# 115 + petit seup
+state="Dome_42chambres_Reference_v4_163"
+# Novermber 2012 state="Dome_42chambres_Reference_v4_115"
+# November 2012 + masks state="Dome_42chambres_Reference_v4_144"
+directory="/data/NAS/February2016"
+monitor='lyosdhcal12.cern.ch'
+daqhost='lyosdhcal9.in2p3.fr'
+daqport=8100
