c2a88379233b2df34a44155f4830669e13cf1400
acqilc <acqilc@localhost.localdomain>
2016-03-29 09:49:08 +0200

    Zup support

--- levbdim_daq/include/LZupServer.hh
+++ levbdim_daq/include/LZupServer.hh
@@ -1,52 +1,26 @@
-#ifndef _RpcZupServer_h
+#ifndef _LZupServer_h
 
-#define _RpcZupServer_h
+#define _LZupServer_h
 #include <iostream>
 
 #include <string.h>
 #include<stdio.h>
-#include "dis.hxx"
+#include "fsm.hh"
 #include "Zup.h"
 using namespace std;
 #include <sstream>
-#include "DaqLogger.h"
-class RpcZupServer;
+#include "LdaqLogger.hh"
 
-class RpcZupOpen : public DimRpc
-{
- public:
-  RpcZupOpen(RpcZupServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcZupServer* _server;
-};
-class RpcZupSwitch : public DimRpc
-{
- public:
-  RpcZupSwitch(RpcZupServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcZupServer* _server;
-};
-class RpcZupRead : public DimRpc
-{
- public:
-  RpcZupRead(RpcZupServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcZupServer* _server;
-};
-
-class RpcZupServer: public DimServer
+class LZupServer
 {
 public:
-  RpcZupServer();
-  void allocateCommands();
-  void setState(std::string s){_state.assign(s);}
-  inline std::string state() const {return _state;}
-  // Publish DIM services
-  inline void publishState(std::string s){setState(s);_zsState->updateService((char*) _state.c_str());}
+  LZupServer(std::string name);
   inline void publishStatus(){_zsStatus->updateService(_status,3*sizeof(float));}
+
+  void configure(levbdim::fsmmessage* m);
+  void on(levbdim::fsmmessage* m);
+  void off(levbdim::fsmmessage* m);
+  void read(levbdim::fsmmessage* m);
   // getters
   void setZup(Zup*z ){_zup =z;}
   Zup* getZup(){return _zup;}
@@ -56,14 +30,9 @@ public:
   void Switch(uint32_t m);
   float* readstatus(){return _status;}
 private:
-  std::string _state;
   float _status[3];
   DimService* _zsStatus; //State of the last register read
-  DimService* _zsState; //State of the last register read 
-  RpcZupOpen* _openCommand;
-  RpcZupSwitch*  _switchCommand;
-  RpcZupRead* _readCommand;
-
+  levbdim::fsm* _fsm;
  
   Zup* _zup;
 };
