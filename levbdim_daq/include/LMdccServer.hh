7df028956abb7e0c1da921c1704cde45263c2c06
acqilc <acqilc@localhost.localdomain>
2016-03-29 15:04:22 +0200

    Mdcc support

--- levbdim_daq/include/LMdccServer.hh
+++ levbdim_daq/include/LMdccServer.hh
@@ -1,104 +1,34 @@
-#ifndef _RpcMdccServer_h
+#ifndef _LMdccServer_h
 
-#define _RpcMdccServer_h
+#define _LMdccServer_h
 #include <iostream>
 
 #include <string.h>
 #include<stdio.h>
-#include "dis.hxx"
+#include "fsm.hh"
 
 #include "MDCCReadout.h"
 
 using namespace std;
 #include <sstream>
-#include "DaqLogger.h"
-class RpcMdccServer;
+#include "LdaqLogger.hh"
 
-class RpcMdccOpen : public DimRpc
-{
- public:
-  RpcMdccOpen(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-
-class RpcMdccClose : public DimRpc
-{
- public:
-  RpcMdccClose(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-class RpcMdccMask : public DimRpc
-{
- public:
-  RpcMdccMask(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-class RpcMdccReset : public DimRpc
-{
- public:
-  RpcMdccReset(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-class RpcMdccStatus : public DimRpc
-{
- public:
-  RpcMdccStatus(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-class RpcMdccWriteReg : public DimRpc
-{
- public:
-  RpcMdccWriteReg(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-
-class RpcMdccReadReg : public DimRpc
-{
- public:
-  RpcMdccReadReg(RpcMdccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcMdccServer* _server;
-};
-
-class RpcMdccServer: public DimServer
+class LMdccServer
 {
 public:
-  RpcMdccServer();
-  void allocateCommands();
+  LMdccServer(std::string name);
+  void open(levbdim::fsmmessage* m);
+  void close(levbdim::fsmmessage* m);
+  void pause(levbdim::fsmmessage* m);
+  void resume(levbdim::fsmmessage* m);
+  void reset(levbdim::fsmmessage* m);
+  void cmd(levbdim::fsmmessage* m);
   void doOpen(std::string s);
-  void setState(std::string s){_state.assign(s);}
-  inline std::string state() const {return _state;}
-  // Publish DIM services
-  inline void publishState(std::string s){setState(s);_csState->updateService((char*) _state.c_str());}
-
-  // getters
 
   MDCCReadout* getMDCCReadout(){  std::cout<<" get Ptr "<<_mdcc<<std::endl;
 return _mdcc;}
 private:
-  std::string _state;
-  DimService* _csState; 
-  RpcMdccOpen* _openCommand;
-  RpcMdccClose*  _closeCommand;
-  RpcMdccMask* _maskCommand;
-  RpcMdccReset*  _resetCommand;
-  RpcMdccStatus* _statusCommand;
-  RpcMdccWriteReg*  _writeregCommand;
-  RpcMdccReadReg* _readregCommand;
-
+  levbdim::fsm* _fsm;
  
   MDCCReadout* _mdcc;
 };
---
+++ levbdim_daq/src/LMdccServer.cc
@@ -0,0 +1,141 @@
+
+#include "LMdccServer.hh"
+void LMdccServer::open(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  std::string device=m->content()["device"].asString();
+  doOpen(device);
+  _mdcc->maskTrigger();
+  _mdcc->resetCounter();
+}
+void LMdccServer::close(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_mdcc==NULL)
+    {
+       LOG4CXX_ERROR(_logLdaq,"Please open MDC01 first");
+       return;
+    }
+  _mdcc->close();
+  _mdcc=NULL;
+}
+void LMdccServer::pause(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_mdcc==NULL)
+    {
+       LOG4CXX_ERROR(_logLdaq,"Please open MDC01 first");
+       return;
+    }
+  _mdcc->maskTrigger();
+}
+void LMdccServer::resume(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_mdcc==NULL)
+    {
+       LOG4CXX_ERROR(_logLdaq,"Please open MDC01 first");
+       return;
+    }
+  _mdcc->unmaskTrigger();
+}
+void LMdccServer::reset(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  if (_mdcc==NULL)
+    {
+       LOG4CXX_ERROR(_logLdaq,"Please open MDC01 first");
+       return;
+    }
+  _mdcc->resetCounter();
+}
+
+
+void LMdccServer::cmd(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  std::string cmd_name=m->content()["name"].asString();
+
+  
+  
+  if (_mdcc==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please open MDC01 first");
+      return;
+    }
+  if (cmd_name.compare("STATUS")==0)
+    {
+      Json::Value rc;
+      rc["version"]=_mdcc->version();
+      rc["id"]=_mdcc->id();
+      rc["mask"]=_mdcc->mask();
+      rc["spill"]=_mdcc->spillCount();
+      rc["busy"]=_mdcc->busyCount();
+      m->setAnswer(rc);
+
+      return;
+    }
+  if (cmd_name.compare("WRITEREG")==0)
+    {
+      uint32_t adr=m->content()["address"].asInt();
+      uint32_t val=m->content()["value"].asInt();
+      _mdcc->writeRegister(adr,val);
+      return;
+    }
+  if (cmd_name.compare("READREG")==0)
+    {
+      uint32_t adr=m->content()["address"].asInt();
+
+      uint32_t val=_mdcc->readRegister(adr);
+      Json::Value r;
+      r["address"]=adr;
+      r["value"]=val;
+      m->setAnswer(r);
+      return;
+    }
+
+}
+
+
+
+
+LMdccServer::LMdccServer(std::string name) : _mdcc(NULL)
+{
+
+  
+  std::stringstream s0;
+  s0.str(std::string());
+  s0<<"LMdccServer-"<<name;
+  DimServer::start(s0.str().c_str()); 
+
+  _fsm=new levbdim::fsm(name);
+
+  
+// Register state
+  _fsm->addState("CREATED");
+  _fsm->addState("PAUSED");
+  _fsm->addState("RUNNING");
+
+  _fsm->addTransition("OPEN","CREATED","PAUSED",boost::bind(&LMdccServer::open, this,_1));
+  _fsm->addTransition("RESUME","PAUSED","RUNNING",boost::bind(&LMdccServer::resume, this,_1));
+  _fsm->addTransition("RESET","PAUSED","PAUSED",boost::bind(&LMdccServer::reset, this,_1));
+  _fsm->addTransition("PAUSE","RUNNING","PAUSED",boost::bind(&LMdccServer::pause, this,_1));
+  _fsm->addTransition("DESTROY","PAUSED","CREATED",boost::bind(&LMdccServer::close, this,_1));
+  
+  _fsm->addTransition("CMD","PAUSED","PAUSED",boost::bind(&LMdccServer::cmd, this,_1));
+  _fsm->addTransition("CMD","RUNNING","RUNNING",boost::bind(&LMdccServer::cmd, this,_1));
+
+	
+
+}
+
+
+void LMdccServer::doOpen(std::string s)
+{
+  std::cout<<"calling open "<<std::endl;
+  if (_mdcc!=NULL)
+    delete _mdcc;
+  _mdcc= new MDCCReadout(s);
+  _mdcc->open();
+  //std::cout<<" Open Ptr "<<_mdcc<<std::endl;
+}
