54750b162c6ca00f6be3a0eff3d60cf700370e6b
acqilc <acqilc@localhost.localdomain>
2016-03-29 10:52:28 +0200

    CCC version

--- levbdim_daq/include/LCccServer.hh
+++ levbdim_daq/include/LCccServer.hh
@@ -1,140 +1,36 @@
-#ifndef _RpcCccServer_h
+#ifndef _LCccServer_h
 
-#define _RpcCccServer_h
+#define _LCccServer_h
 #include <iostream>
 
 #include <string.h>
-#include<stdio.h>
-#include "dis.hxx"
+#include <stdio.h>
+
 
 #include "CCCManager.h"
 
 using namespace std;
 #include <sstream>
-#include "DaqLogger.h"
-class RpcCccServer;
-
-class RpcCccOpen : public DimRpc
-{
- public:
-  RpcCccOpen(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccInitialise : public DimRpc
-{
- public:
-  RpcCccInitialise(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccConfigure : public DimRpc
-{
- public:
-  RpcCccConfigure(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccStart : public DimRpc
-{
- public:
-  RpcCccStart(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccStop : public DimRpc
-{
- public:
-  RpcCccStop(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccPause : public DimRpc
-{
- public:
-  RpcCccPause(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccResume : public DimRpc
-{
- public:
-  RpcCccResume(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccDIFReset : public DimRpc
-{
- public:
-  RpcCccDIFReset(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-class RpcCccCCCReset : public DimRpc
-{
- public:
-  RpcCccCCCReset(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-
-class RpcCccWriteReg : public DimRpc
-{
- public:
-  RpcCccWriteReg(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-
-class RpcCccReadReg : public DimRpc
-{
- public:
-  RpcCccReadReg(RpcCccServer* serv,std::string name);
-  void rpcHandler(); 
- private:
-  RpcCccServer* _server;
-};
-
-class RpcCccServer: public DimServer
+#include "LdaqLogger.hh"
+#include "fsm.hh"
+class LCccServer 
 {
 public:
-  RpcCccServer();
-  void allocateCommands();
+  LCccServer(std::string name);
+  void open(levbdim::fsmmessage* m);
+  void initialise(levbdim::fsmmessage* m);
+  void configure(levbdim::fsmmessage* m);
+  void start(levbdim::fsmmessage* m);
+  void stop(levbdim::fsmmessage* m);
+  void cmd(levbdim::fsmmessage* m);
   void Open(std::string s);
-  void setState(std::string s){_state.assign(s);}
-  inline std::string state() const {return _state;}
-  // Publish DIM services
-  inline void publishState(std::string s){setState(s);_csState->updateService((char*) _state.c_str());}
 
   // getters
 
   CCCManager* getManager(){return _manager;}
 private:
   std::string _state;
-  DimService* _csState; 
-  RpcCccOpen* _openCommand;
-  RpcCccInitialise*  _initialiseCommand;
-  RpcCccConfigure* _configureCommand;
-  RpcCccStart*  _startCommand;
-  RpcCccStop* _stopCommand;
-  RpcCccPause*  _pauseCommand;
-  RpcCccResume* _resumeCommand;
-  RpcCccDIFReset*  _difresetCommand;
-  RpcCccCCCReset* _cccresetCommand;
-  RpcCccWriteReg*  _writeregCommand;
-  RpcCccReadReg* _readregCommand;
-
- 
+  levbdim::fsm* _fsm;
   CCCManager* _manager;
 };
 #endif
---
+++ levbdim_daq/src/LCccServer.cc
@@ -0,0 +1,161 @@
+
+#include "LCccServer.hh"
+
+
+
+void LCccServer::open(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  std::string device=m->content()["device"].asString();
+  this->Open(device);
+}
+
+void LCccServer::initialise(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+
+  CCCManager* ccc= this->getManager();
+  if (ccc==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please open CCC DCCCCC01 first");
+      return;
+    }
+  this->getManager()->initialise();
+}
+void LCccServer::configure(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+
+  CCCManager* ccc= this->getManager();
+  if (ccc==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please open CCC DCCCCC01 first");
+      return;
+    }
+  this->getManager()->configure();
+}
+void LCccServer::start(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+
+  CCCManager* ccc= this->getManager();
+  if (ccc==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please open CCC DCCCCC01 first");
+      return;
+    }
+  this->getManager()->start();
+}
+void LCccServer::stop(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+
+  CCCManager* ccc= this->getManager();
+  if (ccc==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please open CCC DCCCCC01 first");
+      return;
+    }
+  this->getManager()->stop();
+}
+
+void LCccServer::cmd(levbdim::fsmmessage* m)
+{
+  LOG4CXX_INFO(_logLdaq," CMD: "<<m->command());
+  std::string cmd_name=m->content()["name"].asString();
+
+  
+  CCCManager* ccc= this->getManager();
+  if (ccc==NULL)
+    {
+      LOG4CXX_ERROR(_logLdaq,"Please open CCC DCCCCC01 first");
+      return;
+    }
+  if (cmd_name.compare("PAUSE")==0)
+    {
+      ccc->getCCCReadout()->DoSendPauseTrigger();
+      return;
+    }
+  if (cmd_name.compare("RESUME")==0)
+    {
+      ccc->getCCCReadout()->DoSendResumeTrigger();
+      return;
+    }
+  if (cmd_name.compare("DIFRESET")==0)
+    {
+      ccc->getCCCReadout()->DoSendDIFReset();
+      return;
+    }
+  if (cmd_name.compare("CCCRESET")==0)
+    {
+      ccc->getCCCReadout()->DoSendCCCReset();
+      return;
+    }
+  if (cmd_name.compare("WRITEREG")==0)
+    {
+      uint32_t adr=m->content()["address"].asInt();
+      uint32_t val=m->content()["value"].asInt();
+      ccc->getCCCReadout()->DoWriteRegister(adr,val);
+      return;
+    }
+  if (cmd_name.compare("READREG")==0)
+    {
+      uint32_t adr=m->content()["address"].asInt();
+
+      uint32_t val=ccc->getCCCReadout()->DoReadRegister(adr);
+      Json::Value r;
+      r["address"]=adr;
+      r["value"]=val;
+      m->setAnswer(r);
+      return;
+    }
+
+}
+
+
+ 
+ 
+
+
+
+
+ LCccServer::LCccServer(std::string name) : _manager(NULL)
+{
+  _fsm=new levbdim::fsm(name);
+
+  
+// Register state
+  _fsm->addState("CREATED");
+  _fsm->addState("CONFIGURED");
+  _fsm->addState("ON");
+  _fsm->addState("OFF");
+
+  _fsm->addTransition("OPEN","CREATED","OPENED",boost::bind(&LCccServer::open, this,_1));
+  _fsm->addTransition("INITIALISE","OPENED","INITIALISED",boost::bind(&LCccServer::initialise, this,_1));
+  _fsm->addTransition("CONFIGURE","INITIALISED","CONFIGURED",boost::bind(&LCccServer::configure, this,_1));
+  _fsm->addTransition("START","CONFIGURED","RUNNING",boost::bind(&LCccServer::start, this,_1));
+  _fsm->addTransition("STOP","RUNNING","CONFIGURED",boost::bind(&LCccServer::stop, this,_1));
+  _fsm->addTransition("DESTROY","CONFIGURED","OPENED",boost::bind(&LCccServer::open, this,_1));
+  
+  _fsm->addTransition("CMD","OPENED","OPENED",boost::bind(&LCccServer::cmd, this,_1));
+  _fsm->addTransition("CMD","INITIALISED","INITIALISED",boost::bind(&LCccServer::cmd, this,_1));
+  _fsm->addTransition("CMD","CONFIGURED","CONFIGURED",boost::bind(&LCccServer::cmd, this,_1));
+
+  
+  std::stringstream s0;
+  s0.str(std::string());
+  s0<<"LCccServer-"<<name;
+  DimServer::start(s0.str().c_str()); 
+
+
+	
+
+}
+
+
+void LCccServer::Open(std::string s)
+{
+  if (_manager!=NULL)
+    delete _manager;
+  _manager= new CCCManager(s);
+}
