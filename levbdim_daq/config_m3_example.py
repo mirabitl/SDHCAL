fbf3274de624d9ecaef60e58a659a6b9b622c4fd
acqilc <acqilc@localhost.localdomain>
2016-04-03 08:33:49 +0200

    web version

---
+++ levbdim_daq/config_m3_example.py
@@ -0,0 +1,16 @@
+jsonfile='/data/NAS/config/levbdim_m3.json'
+zupdevice='/dev/ttyUSB0'
+zupport=1
+dccname="DCCCCC01"
+mdccname="MDCC01"
+
+ctrlreg=0x89580000
+# 115 + petit seup
+dbstate="Dome_42chambres_Reference_v4_163"
+# Novermber 2012 state="Dome_42chambres_Reference_v4_115"
+# November 2012 + masks state="Dome_42chambres_Reference_v4_144"
+filepath="/data/NAS/February2016"
+memorypath="/dev/shm/levbdim"
+proctype="lcio"
+daqhost='lyosdhcal9.in2p3.fr'
+daqport=8200
