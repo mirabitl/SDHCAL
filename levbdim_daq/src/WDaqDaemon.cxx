ac302f8ef90a19e888a3b512c16740169e1a7522
acqilc <acqilc@localhost.localdomain>
2016-04-30 08:59:29 +0200

    daemon missed

---
+++ levbdim_daq/src/WDaqDaemon.cxx
@@ -0,0 +1,26 @@
+#include "WebDaq.hh"
+#include "LdaqLogger.hh"
+#include <unistd.h>
+#include <stdint.h>
+#include <log4cxx/logger.h>
+#include <log4cxx/xml/domconfigurator.h>
+using namespace log4cxx;
+using namespace log4cxx::xml;
+using namespace log4cxx::helpers;
+
+int main()
+{
+printf("parsing the config file \n");
+DOMConfigurator::configure("/etc/Log4cxxConfig.xml");
+//_logger->setLevel(log4cxx::Level::getInfo());
+LOG4CXX_INFO (_logLdaq, "this is a info message, after parsing configuration file")
+  std::stringstream s0;
+  char hname[80];
+  gethostname(hname,80);
+  s0<<"WD-"<<hname;
+
+
+  WebDaq* s=new WebDaq("WDAQ",45000);
+  while (true)
+    sleep((unsigned int) 3);
+}
