aae7a397cb3a5c39d3420ee2a67e6447bbef3aaa
acqilc <acqilc@localhost.localdomain>
2016-05-06 14:15:28 +0200

    curl used by system call

--- WtWizard/wlevb.C
+++ WtWizard/wlevb.C
@@ -43,82 +43,6 @@ using namespace  std;
 #include <json/json.h>
 //using namespace Wt;
  #define STEP printf("%s %d\n",__FUNCTION__,__LINE__)
-using namespace std;
-#include <curl/curl.h>
-//#include <curl/types.h>
-#include <curl/easy.h>
-
-struct CurlBufferStruct
-{
-  char* buffer;
-  size_t size;
-};
- static size_t CurlCallback(void* ptr, size_t size, size_t nmemb, void* data)
-{
-  size_t realsize = size * nmemb;
-  struct CurlBufferStruct* mem = (struct CurlBufferStruct*) data;
-  mem->buffer = (char*)realloc(mem->buffer, mem->size + realsize + 1);
- 
-  if ( mem->buffer )
-  {
-    memcpy(&(mem->buffer[mem->size]), ptr, realsize );
-    mem->size += realsize;
-    mem->buffer[ mem->size ] = 0;
-  }
- 
-  return realsize;
-}
- 
- 
-//Lecture de la page web
-  char* CurlQuery(char* AddURL,char* Chaine)
-  {
-  curl_global_init(CURL_GLOBAL_ALL);
-  
-  CURL *myHandle;
-  CURLcode result;
-  struct CurlBufferStruct LectureLC;
-  LectureLC.buffer = NULL;
-  LectureLC.size = 0;
- 
-  myHandle = curl_easy_init();
-  curl_easy_setopt(myHandle, CURLOPT_WRITEFUNCTION, CurlCallback);
-  curl_easy_setopt(myHandle, CURLOPT_WRITEDATA, (void*)&LectureLC);
-  curl_easy_setopt(myHandle, CURLOPT_URL, AddURL);
-  result = curl_easy_perform(myHandle);  //voir la doc pour une gestion minimal des erreurs
-  curl_easy_cleanup(myHandle);
- 
-  if(result!=0) LectureLC.size=1;
-
-  strcpy(Chaine, LectureLC.buffer);  strcat(Chaine,"\0");
-  if(LectureLC.buffer) free(LectureLC.buffer);
- 
-  return Chaine;
-  }
-
-
-string url_encode(const string &value) {
-    ostringstream escaped;
-    escaped.fill('0');
-    escaped << hex;
-
-    for (string::const_iterator i = value.begin(), n = value.end(); i != n; ++i) {
-        string::value_type c = (*i);
-
-        // Keep alphanumeric and other accepted characters intact
-        if (isalnum(c) || c == '-' || c == '_' || c == '.' || c == '~') {
-            escaped << c;
-            continue;
-        }
-
-        // Any other characters are percent-encoded
-        escaped << uppercase;
-        escaped << '%' << setw(2) << int((unsigned char) c);
-        escaped << nouppercase;
-    }
-
-    return escaped.str();
-}
 /*
  * A simple hello world application class which demonstrates how to react
  * to events, read input, and give feed-back.
@@ -1225,16 +1149,16 @@ void wlevbc::StartButtonHandler()
 	    std::string resl=exec((const char*) telog);
 	    currentElog_=atoi(resl.substr(resl.find("ID=")+3,10).c_str());
 	    currentSessionId_=this->sessionId();
-
+	    // Publish SOR to monitoring
 	    std::stringstream s,saction;
 	    s<<"curl -X POST http://lyoac29:8082/SDHCALRunControl/SOR ";
 	    saction<<" -d run="<<editrun->text().toUTF8();
-	    saction<<" -d detectorName="<<editstate->text().toUTF8();
-	    saction<<" -d author="<<editauthor->text().toUTF8();
-	    saction<<" -d beam="<<editbeam->text().toUTF8();
-	    saction<<" -d energy="<<editenergy->text().toUTF8();
-	    saction<<" -d description="<<editcomment->text().toUTF8();
-	    s<<saction;
+	    saction<<" -d detectorName=\""<<editstate->text().toUTF8()<<"\"";
+	    saction<<" -d author=\""<<editauthor->text().toUTF8()<<"\"";
+	    saction<<" -d beam=\""<<editbeam->text().toUTF8()<<"\"";
+	    saction<<" -d energy=\""<<editenergy->text().toUTF8()<<"\"";
+	    saction<<" -d description=\""<<editcomment->text().toUTF8()<<"\"";
+	    s<<saction.str();
 	    std::cout<<"Web command "<<s.str()<<std::endl;
 	    std::string resm=exec(s.str().c_str());
 	    std::cout<<resm<<std::endl;
@@ -1261,6 +1185,17 @@ void wlevbc::StopButtonHandler()
       std::string resl=exec((const char*) telog);
       currentElog_=atoi(resl.substr(resl.find("ID=")+3,10).c_str());
     }
+  // Publish EOR anywayto monitoring
+  std::stringstream s,saction;
+  s<<"curl -X POST http://lyoac29:8082/SDHCALRunControl/EOR ";
+  saction<<" -d events="<<currentEvent_;
+  s<<saction.str();
+  std::cout<<"Web command "<<s.str()<<std::endl;
+  std::string resm=exec(s.str().c_str());
+  std::cout<<resm<<std::endl;
+
+      
+    
   PB_Start_->enable();
   PB_Stop_->disable();
   PB_Stop_->disable();
