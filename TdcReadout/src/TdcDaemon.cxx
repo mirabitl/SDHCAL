3636df7dc83fce69a6872790b4a65c6afb05515c
mirabitl <mirabito@cern.ch>
2017-03-28 11:21:57 +0200

    INSTANCE usage

--- TdcReadout/src/TdcDaemon.cxx
+++ TdcReadout/src/TdcDaemon.cxx
@@ -14,9 +14,11 @@ printf("parsing the config file \n");
 //_logger->setLevel(log4cxx::Level::getInfo());
 //LOG4CXX_INFO (_logLdaq, "this is a info message, after parsing configuration file")
   std::stringstream s0;
-  char hname[80];
-  gethostname(hname,80);
-  s0<<"TDC-"<<hname;
+  uint32_t instance=0;
+  char* wp=getenv("INSTANCE");
+  if (wp!=NULL)      instance=atoi(wp);
+  s0<<"TDC-"<<instance;
+  std::cout<<"Starting  "<<s0.str()<<std::endl;
 
 
   TdcManager* s=new TdcManager(s0.str());
