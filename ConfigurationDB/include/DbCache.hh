c39ba26379db475de52cab2b8470fc228b12aba9
Lagarde François <lagarde@ipnl.in2p3.fr>
2017-04-03 11:23:24 +0200

    updated version

--- ConfigurationDB/include/DbCache.hh
+++ ConfigurationDB/include/DbCache.hh
@@ -1,3 +1,21 @@
 #ifndef _DBCACHE_HH
 #define _DBCACHE_HH
+#include "OracleDIFDBInterface.hh"
+#include "baseApplication.hh"
+class DbCache : public levbdim::baseApplication
+{
+public:
+  DbCache(std::string name);
+  void configure(levbdim::fsmmessage* m);
+  void destroy(levbdim::fsmmessage* m);
+  // action
+  void c_status(Mongoose::Request &request, Mongoose::JsonResponse &response);
+private:
+  void ls(std::string sourcedir,std::vector<std::string>& res);
+  
+  std::string _mode;
+  std::string _stateName;
+  std::string _path;
+  State* _state;
+};
 #endif
